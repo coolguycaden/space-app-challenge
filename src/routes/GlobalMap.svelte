59496dfc2d84f13362f4e9746225f5aa208d826c
anabasis <anabasis@noreply.codeberg.org>
2025-10-04 11:25:10 -0400

    feat: add svelte-maplibre basics

---
+++ src/routes/GlobalMap.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	// @ts-ignore
+	import MapLibre from "svelte-maplibre/MapLibre.svelte";
+	import type { LngLatBoundsLike } from "maplibre-gl";
+
+	let bounds: LngLatBoundsLike = $state([-32, -8, 63, 41]);
+	let displayBounds = $derived(bounds.map((b) => b.toFixed(4)).join(", "));
+</script>
+
+<p class="tabular-nums">Bounds: {displayBounds}</p>
+
+<MapLibre
+	style="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
+	class="relative aspect-[9/16] max-h-[70vh] w-full sm:aspect-video sm:max-h-full"
+	standardControls
+	bind:bounds
+/>
