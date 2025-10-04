e6309848d4105f1780cd63f18d37552f33c0b018
Adrian Colaianni <adrian@colaianni.us>
2025-10-04 19:25:48 -0400

    Added selectedAsteroid

--- src/routes/SimulationSettings.svelte
+++ src/routes/SimulationSettings.svelte
@@ -10,39 +10,18 @@
     let data: PageData = $props();
     let asteroids: Asteroid[] = data.asteroids;
 
-    let selectedAsteroid = $state("");
+    let selectedAsteroidName = $state("");
     const countryTriggerContent = $derived(
-        asteroids.find((c) => c.fullname === selectedAsteroid)?.fullname ??
+        asteroids.find((c) => c.fullname === selectedAsteroidName)?.fullname ??
             "Select an asteroid",
     );
-
-    let meteor = $state({
-        size: 42,
-        velocity: 67,
-    });
-
-    let meteorPresets: { [id: string]: number } = {
-        Demi: 8,
-        Short: 20,
-        Tall: 30,
-        Grande: 40,
-        Venti: 60,
-        Trenta: 75,
-        "BIG BERTHA": 100,
-    };
-
-    let selectedMeteorPreset = $state("Grande");
-
-    function onMeteorPresetChange(value: string) {
-        meteor.size = meteorPresets[value];
-        meteor.velocity = meteorPresets[value];
-    }
-
-    function onCustomMeteor() {
-        selectedMeteorPreset = "Custom";
-    }
-
-    let toggle = $state(false);
+    let selectedAsteroid: Asteroid = $derived(
+        asteroids.find((c) => c.fullname === selectedAsteroidName) ?? {
+            velocity: 0,
+            fullname: "",
+            diameter: 0,
+        }
+    );
 
 </script>
 
@@ -51,7 +30,7 @@
 
     <div class="flex flex-row gap-10">
         <p>Select Asteroid</p>
-        <Select.Root type="single" name="asteroid" bind:value={selectedAsteroid}>
+        <Select.Root type="single" name="asteroid" bind:value={selectedAsteroidName}>
             <Select.Trigger class="w-[180px]">
                 {countryTriggerContent}
             </Select.Trigger>
@@ -69,60 +48,10 @@
             </Select.Content>
         </Select.Root>
     </div>
-
+    <p>Asterid Stats</p>
+    <p>Velocity: {selectedAsteroid.velocity}</p>
+    <p>Diameter: {selectedAsteroid.diameter}</p>
     <div class="flex flex-row gap-10">
-        <p>Meteor Preset</p>
-        <Select.Root
-            type="single"
-            name="country"
-            bind:value={selectedMeteorPreset}
-            onValueChange={onMeteorPresetChange}
-        >
-            <Select.Trigger class="w-[180px]">
-                {selectedMeteorPreset}
-            </Select.Trigger>
-            <Select.Content>
-                <Select.Group>
-                    {#each Object.entries(meteorPresets) as meteor}
-                        <Select.Item value={meteor[0]}>
-                            {meteor[0]}
-                        </Select.Item>
-                    {/each}
-                </Select.Group>
-            </Select.Content>
-        </Select.Root>
     </div>
 
-    <div class="flex flex-row gap-10">
-        <Label for="amount">Asteroid Size</Label>
-        <!-- TODO: bind -->
-        <Slider
-            id="amount"
-            type="single"
-            max={100}
-            step={1}
-            class="max-w-[100%]"
-            bind:value={meteor.size}
-            onValueChange={onCustomMeteor}
-        />
-    </div>
-
-    <div class="flex flex-row gap-10">
-        <Label for="amount">Asteroid Velocity</Label>
-        <!-- TODO: bind -->
-        <Slider
-            id="amount"
-            type="single"
-            max={100}
-            step={1}
-            class="max-w-[100%]"
-            bind:value={meteor.velocity}
-            onValueChange={onCustomMeteor}
-        />
-    </div>
-
-    <div class="flex flex-row gap-10">
-        <Label>Toggle</Label>
-        <Switch id="toggle" bind:checked={toggle} />
-    </div>
 </div>
