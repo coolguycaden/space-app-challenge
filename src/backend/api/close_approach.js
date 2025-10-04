5b8cc801e94a2b4e5a1809a09c78cc4a4e83227e
Caden Allen <cadenallen97@gmail.com>
2025-10-04 11:50:28 -0400

    starting api for sdbd close approach

---
+++ src/backend/api/close_approach.js
@@ -0,0 +1,34 @@
+const SDBD_CLOSE_APPROACH_API = 'https://ssd-api.jpl.nasa.gov/cad.api'
+
+// Response request options
+const REQUEST_OPTIONS = {
+    method: 'GET',
+    headers: {
+        'Authorization': 'any'
+    },
+
+};
+
+//Make the request
+fetch(SDBD_CLOSE_APPROACH_API, REQUEST_OPTIONS)
+
+    //Check if response is good
+    .then(response => {
+        if(!response.ok) {
+            throw new Error('Network response was not ok.');
+        }
+
+        return response.json();
+    })
+
+    .then(requestData => {
+        const {count, fields, data} = requestData;
+        const {dist, dist_min, dist_max} = data
+    })
+
+    .catch(error => {
+        console.error('Error: ', error)
+    })
+
+
+
