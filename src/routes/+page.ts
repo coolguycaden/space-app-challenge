a4555ab727eec558762e99a2d4732a8a9d678448
anabasis <anabasis@noreply.codeberg.org>
2025-10-04 21:01:04 -0400

    style: fix formatting

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,7 +1,6 @@
-import { error } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ parent, data }) => {
-    await parent();
-    return data;
+	await parent();
+	return data;
 };
